352f6faf4fd99169819728d777f65f7efdcd04c0
Radek Micek <radek.micek@gmail.com>
2013-04-08 14:06:12 +0200

    Evaluate results of Idris callbacks

--- src/D3Helper.js
+++ src/D3Helper.js
@@ -18,17 +18,34 @@ var decodeEntities = (function() {
   return decodeInternal;
 })();
 
+var EVAL = function (a0) {
+  var ev = __IDRRT__.tailcall(function () {
+    return __IDR__.EVAL0(a0);
+  });
+  return ev;
+};
+
+var APPLY1 = function (f, a0) {
+  var ap = __IDRRT__.tailcall(function () {
+    return __IDR__.APPLY0(f, a0);
+  });
+  return EVAL(ap);
+};
+
+var APPLY2 = function (f, a0, a1) {
+  var ap = __IDRRT__.tailcall(function () {
+    return __IDR__.APPLY0(__IDR__.APPLY0(f, a0), a1);
+  });
+  return EVAL(ap);
+};
+
 var promptHelper = function (msg, mkNothing, mkJust) {
   var result = prompt(msg);
   if (result === null) {
-    return __IDRRT__.tailcall(function () {
-      return __IDR__.APPLY0(mkNothing, undefined);
-    });
+    return APPLY1(mkNothing, undefined);
   }
   else {
-    return __IDRRT__.tailcall(function () {
-      return __IDR__.APPLY0(mkJust, result);
-    });
+    return APPLY1(mkJust, result);
   }
 };
 
@@ -42,9 +59,7 @@ var setNth = function (arr, i, val) {
 
 var anyA = function (f, arr) {
   var res = arr.some(function (x) {
-    return __IDRRT__.tailcall(function () {
-      return __IDR__.APPLY0(f, x);
-    });
+    return APPLY1(f, x);
   });
 
   return res ? 1 : 0;
@@ -52,9 +67,7 @@ var anyA = function (f, arr) {
 
 var filterA = function (f, arr) {
   return arr.filter(function (x) {
-    return __IDRRT__.tailcall(function () {
-      return __IDR__.APPLY0(f, x);
-    });
+    return APPLY1(f, x);
   });
 };
 
@@ -64,88 +77,66 @@ var d3Root = function () {
 
 var attrPrime = function (sel, attr, f) {
   return sel.attr(attr, function (d, i) {
-    return __IDRRT__.tailcall(function () {
-      return __IDR__.APPLY0(__IDR__.APPLY0(f, d), i);
-    });
+    return APPLY2(f, d, i);
   });
 };
 
 var classedPrime = function (sel, cls, f) {
   return sel.classed(cls, function (d, i) {
-    return __IDRRT__.tailcall(function () {
-      return __IDR__.APPLY0(__IDR__.APPLY0(f, d), i);
-    });
+    return APPLY2(f, d, i);
   });
 };
 
 var stylePrime = function (sel, name, f) {
   return sel.style(name, function (d, i) {
-    return __IDRRT__.tailcall(function () {
-      return __IDR__.APPLY0(__IDR__.APPLY0(f, d), i);
-    });
+    return APPLY2(f, d, i);
   });
 };
 
 var propertyPrime = function (sel, name, f) {
   return sel.property(name, function (d, i) {
-    return __IDRRT__.tailcall(function () {
-      return __IDR__.APPLY0(__IDR__.APPLY0(f, d), i);
-    });
+    return APPLY2(f, d, i);
   });
 };
 
 var textPrime = function (sel, f) {
   return sel.text(function (d, i) {
-    return __IDRRT__.tailcall(function () {
-      return __IDR__.APPLY0(__IDR__.APPLY0(f, d), i);
-    });
+    return APPLY2(f, d, i);
   });
 };
 
 var htmlPrime = function (sel, f) {
   return sel.html(function (d, i) {
-    return __IDRRT__.tailcall(function () {
-      return __IDR__.APPLY0(__IDR__.APPLY0(f, d), i);
-    });
+    return APPLY2(f, d, i);
   });
 };
 
 var bindPrime = function (sel, f) {
   return sel.data(function (d, i) {
-    return __IDRRT__.tailcall(function () {
-      return __IDR__.APPLY0(__IDR__.APPLY0(f, d), i);
-    });
+    return APPLY2(f, d, i);
   });
 };
 
 var bindK = function (sel, arr, key) {
   return sel.data(arr, function (d, i) {
-    return __IDRRT__.tailcall(function () {
-      return __IDR__.APPLY0(__IDR__.APPLY0(key, d), i);
-    });
+    return APPLY2(key, d, i);
   });
 };
 
 var bindKPrime = function (sel, f, key) {
   return sel.data(
     function (d, i) {
-      return __IDRRT__.tailcall(function () {
-        return __IDR__.APPLY0(__IDR__.APPLY0(f, d), i);
-      });
+      return APPLY2(f, d, i);
     },
     function (d, i) {
-      return __IDRRT__.tailcall(function () {
-        return __IDR__.APPLY0(__IDR__.APPLY0(key, d), i);
-      });
+      return APPLY2(key, d, i);
     }
   );
 };
 
 var onClick = function (sel, handler) {
   sel.on("click", function () {
-    return __IDRRT__.tailcall(function () {
-      return __IDR__.APPLY0(handler, undefined);
-    });
+    return APPLY1(handler, undefined);
   });
 };
 
@@ -171,9 +162,7 @@ var getHeightL = function (fl) {
 
 var onTickL = function (fl, handler) {
   fl.on("tick", function () {
-    return __IDRRT__.tailcall(function () {
-      return __IDR__.APPLY0(handler, undefined);
-    });
+    return APPLY1(handler, undefined);
   });
 };
 
